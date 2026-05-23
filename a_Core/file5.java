04fa8a99b5ca403bd45f518f56811bd424101f0d
D-Bhattacherjee <bhattacherjeedebojyoti4@gmail.com>
2026-05-23 10:00:06 +0530

    Loops(file5)-forloop, whileloop

---
+++ a_Core/file5.java
@@ -0,0 +1,25 @@
+public class file5 {
+    public static void main(String[] args) {
+        // looping with for loop
+        int limit = 10;
+
+        // printing from 0 to 10
+        /*
+         * for(initiation of variable; run untill condition; increase or decreasevalue){
+         * code-to-execute
+         * }
+         */
+        for (int i = 0; i <= limit; i++) {
+            System.out.println(i);
+        }
+        System.out.println("=============================");
+        // another loop While
+        int j = 0;
+        while (j <= 10) {
+
+            j += 1; // it means j = j + 1
+            System.out.println(j);
+        }
+
+    }
+}
