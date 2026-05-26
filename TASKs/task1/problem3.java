eb3e5bd0204cc7a4688e5b17e7f6edb989d717c3
D-Bhattacherjee <bhattacherjeedebojyoti4@gmail.com>
2026-05-26 13:01:14 +0530

    Task -1 DONE!

--- TASKs/task1/problem3.java
+++ TASKs/task1/problem3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * PROBLEM STATEMENT 3 - Take input of a digit and check if it is Positive or
  * not
@@ -5,6 +7,15 @@
 
 public class problem3 {
     public static void main(String[] args) {
-        System.out.println("YO");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number to check if it is Positive :\t");
+        int digit = sc.nextInt();
+
+        if (digit >= 0) {
+            System.out.println(digit + " is positive");
+        } else {
+            System.out.println(digit + " is negative");
+        }
+        sc.close();
     }
 }
--- TASKs/task1/problem5.java
+++ TASKs/task1/problem5.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 /**
  * PROBLEM STATEMENT 5- Count Nth term to 0
  */
 public class problem5 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number to start counting toward 0 :\t");
+        int Nth = sc.nextInt();
+        sc.close();
 
+        for (int i = Nth; i >= 0; --i) {
+            System.out.println("Count :\t" + i);
+        }
     }
 }
