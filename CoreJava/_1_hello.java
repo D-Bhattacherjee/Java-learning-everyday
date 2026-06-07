23de1594b5b98837ac89d0142e1f477460a1f0a4
D-Bhattacherjee <bhattacherjeedebojyoti4@gmail.com>
2026-06-07 14:38:16 +0530

    hello world

--- CoreJava/_1_hello.java
+++ CoreJava/_1_hello.java
@@ -1,6 +1,9 @@
-// Basic Print Hello World
-public class file1 {
+package CoreJava;
+
+public class _1_hello {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
+// with package declaration
