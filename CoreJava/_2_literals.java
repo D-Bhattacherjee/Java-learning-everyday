dfded1d297a4929b9760065c000b265883d1b3f0
D-Bhattacherjee <bhattacherjeedebojyoti4@gmail.com>
2026-06-07 14:53:20 +0530

    literals

---
+++ CoreJava/_2_literals.java
@@ -0,0 +1,16 @@
+package CoreJava;
+
+public class _2_literals {
+    public static void main(String[] args) {
+        // Literals
+        int num1 = 9;
+        int num2 = 0b01101;
+        int num3 = 0xE;
+        int num4 = 9;
+
+        System.out.println("num1" + num1);
+        System.out.println("num2" + num2);
+        System.out.println("num3" + num3);
+        System.out.println(num4);
+    }
+}
