648dfa37ec70a7399550f12a4aa8bcc5e7a7ff0d
D-Bhattacherjee <bhattacherjeedebojyoti4@gmail.com>
2026-06-11 12:28:19 +0530

    loops

---
+++ CoreJava/_5_loops1.java
@@ -0,0 +1,16 @@
+package CoreJava;
+
+public class _5_loops1 {
+    public static void main(String[] args) {
+
+        int count = 0;
+        while (count < 10) {
+            count++;
+            System.out.println(count);
+        }
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
--- CoreJava/_5_relationalOP.java
+++
@@ -1,7 +0,0 @@
-package CoreJava;
-
-public class _5_relationalOP {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
